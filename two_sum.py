a7f5a63839a1f732931505ea20f05e3ec29f4867
Victoria Le <leqvictoria@gmail.com>
2023-12-22 10:31:21 -0500

    first problem

--- two_sum.py
+++ two_sum.py
@@ -1,1 +1,3 @@
-# Problem: Given an array of numbs and a target integer target, return indices of the two numbers such as they add up to the target. 
+# Given a array of intergers nums and an interger target, return indices of the two numbers such that they add uo to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice. 
+# You can return the answer in any order. 
