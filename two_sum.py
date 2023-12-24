6c3e14346c8defbc4410fe83d4a86b7189ad20d9
Victoria Le <leqvictoria@gmail.com>
2023-12-23 20:24:01 -0500

    step 1

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,15 @@
+# PROBLEM
 # Given a array of intergers nums and an interger target, return indices of the two numbers such that they add uo to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice. 
 # You can return the answer in any order. 
+# 
+
+nums = [1,25,4,5,6,8,3]
+target = 6 
+
+numMAp = {}
+n = len(nums)
+
+for i in range(n):
+    numMAp[nums[i]] = i #assign each number in the num array into a dictionary whose key is the actual number and the value is their indice. 
+print(numMAp)
